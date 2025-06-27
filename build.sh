20fb31757b75d811e3a5e888c33a87f7aade7df5
Christoph Stoettner <christoph.stoettner@stoeps.de>
2025-06-27 11:02:39 +0200

    local changes

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 # Build script for Tuxedo drivers
 # Builts: tuxedo-drivers-kmod-common, kmod-tuxedo-drivers (for your kernel) and akmod-tuxedo-drivers
 
+set -e
+
+if [ ! -d ~/rpmbuild/SPECS ] ; then
+  mkdir -p ~/rpmbuild/SPECS
+fi
+
+for prg in spectool rpmbuild kmodtool ; do
+  which $prg 2>/dev/null || echo "Install $prg to create the build files"
+done
+
 echo "--> Copying spec files to ~/rpmbuild/SPECS/"
 cp ./tuxedo-drivers-kmod.spec ~/rpmbuild/SPECS/
 cp ./tuxedo-drivers-kmod-common.spec ~/rpmbuild/SPECS/
@@ -11,7 +21,7 @@ cd ~/rpmbuild/SPECS/
 echo "--> Installing dependencies"
 spectool -g -R tuxedo-drivers-kmod.spec
 
-#rm ~/rpmbuild/RPMS/* -rf
+# rm ~/rpmbuild/RPMS/x86_64/* -f
 
 echo "--> Building RPMs"
 rpmbuild -ba tuxedo-drivers-kmod-common.spec
